c8653ae39f6686d3c7adf17512b2c1ebbb1f5d0c
Raphael Barboza <eletronic.raphael@gmail.com>
2024-02-01 15:31:10 -0400

    fix: E-mail address column not unique.

---
+++ database/migrations/2024_02_01_192455_update_contacts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->string('email_address',100)->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->dropUnique('contacts_email_address_unique');
+            $table->string('email_address',100)->change();
+        });
+    }
+};
